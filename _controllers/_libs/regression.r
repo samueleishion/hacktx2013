c1af53f9febc2b6300680a257866605479e6bc7c
Julie Bielamowicz <juls_loves_hello_kitty@wireless-10-146-102-201.public.utexas.edu>
2013-11-16 00:49:25 -0600

    regression file

---
+++ _controllers/_libs/regression.r
@@ -0,0 +1,15 @@
+#!/bin/bash
+# r --slave -f regression.r
+#
+file = 
+df = read.table(file, header = T, sep = "\t")
+x1 = df$
+sentiment_P = df$
+sentiment_N = 
+sentiment_E = df
+
+
+regress_adjP = cor(x, y, method = "pearson")
+regress_adjN = cor(x, y, method = "pearson")
+regress_adjE = cor(x, y, method = "pearson")
+
